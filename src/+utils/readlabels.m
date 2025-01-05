9ef04b925f16428458d851f7c7d6ebc58ba747e7
lfx <lordfighterx@gmail.com>
2025-01-05 20:45:29 +0100

    replace path concat with fullfile

--- src/+utils/readlabels.m
+++ src/+utils/readlabels.m
@@ -2,6 +2,6 @@ function [images, labels] = readlabels(path, folder)
 %READLABELS Read labels from a csv file, returning images path and the corresponding labels
 
 rows = readmatrix(path, 'OutputType', 'string', 'NumHeaderLines', 1, 'Delimiter', ',');
-images = folder + rows(:, 1);
+images = fullfile(folder, rows(:, 1));
 labels = rows(:, 2);
 end
