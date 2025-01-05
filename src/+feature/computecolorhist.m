9a9a2419f7114e439f92a3e1166491812977232f
lfx <lordfighterx@gmail.com>
2025-01-05 20:50:23 +0100

    improve feature computation time

--- src/+feature/computecolorhist.m
+++ src/+feature/computecolorhist.m
@@ -1,45 +1,43 @@
 function out = computecolorhist(image, N, n_bins)
 %COMPUTECOLORHIST Compute the local color histogram of an image with N blocks.
 
-S = size(image);
-
-if S(2) > 2
-    [r, c, ch] = size(image);
-else
-    [r, c] = size(image);
-    ch = 1;
-end
+[r, c, ~] = size(image);
 
 if (N > r || N > c)
-    image = reshape(image, [], ch);
     out = colorhist(image, n_bins);
 else
-    out = [];
-    for i=1:N:r
-        for j=1:N:c
-            if (((i+N-1) > r) || ((j+N-1) > c))
-                continue
-            end
+    num_blocks = floor(r / N) * floor(c / N);
+    out = zeros(num_blocks, sum(n_bins));
 
+    block_idx = 1;
+    for i=1:N:r-N+1
+        for j=1:N:c-N+1
             patch = image(i:(i+N-1), j:(j+N-1), :);
-            patch = reshape(patch, [], ch);
-            out = [out, colorhist(patch, n_bins)];
+
+            out(block_idx, :) = colorhist(patch, n_bins);
+            block_idx = block_idx + 1;
         end
     end
 end
+
+out = reshape(out, 1, []);
 
 end
 
 function H = colorhist(im, n_bins)
 %COLOR_HIST Compute the histogram of an image in each color channel
 
-im = reshape(im, [], 3);
+[~, ~, ch] = size(im);
 
-H = [];
-for ch=1:3
-    tmp = hist(im(:,ch), linspace(0, 1, n_bins(ch)));
+idx = 1;
+H = zeros(sum(n_bins), 1);
+
+for i=1:ch
+    tmp = imhist(im(:, :, i), n_bins(i));
     tmp = tmp ./ sum(tmp);
-    H = [H tmp];
+
+    H(idx:idx + n_bins(i) - 1) = tmp;
+    idx = idx + n_bins(i);
 end
 
 end
